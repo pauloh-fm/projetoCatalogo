ca3ea3f77afdf08eede49a8a218b6786897e54e5
pauloh-fm <phfarmar@gmail.com>
2023-03-02 02:39:39 -0300

    lista de produtos

---
+++ script.js
@@ -0,0 +1,38 @@
+const item_list = [
+    {
+      index: 1,
+      category: "Itens de Papelaria",
+      subitems: [
+        {
+          index: 1,
+          name: "Caderno",
+          description: "Descrição do caderno...",
+          image: "https://dl.dropbox.com/s/px6nlikdscdqy8c/ab_caderno.png"
+        },
+        {
+          index: 2,
+          name: "Lápis",
+          description: "Descrição do lápis...",
+          image: "https://dl.dropbox.com/s/qr6bi3xu06hkk25/a_lapis.png"
+        }
+      ]
+    },
+    {
+      index: 2,
+      category: "Itens de Limpeza",
+      subitems: [
+        {
+          index: 3,
+          name: "Detergente",
+          description: "Descrição do detergente...",
+          image: "https://dl.dropbox.com/s/dauiby00b2ageer/a_detergente.png"
+        },
+        {
+          index: 4,
+          name: "Sabão em pedra",
+          description: "Descrição do sabão em pedra...",
+          image: "https://dl.dropbox.com/s/sts4mc0w7hqw1xu/a_sabaopedra.png"
+        }
+    ]
+  },
+]
